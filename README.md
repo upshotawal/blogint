dc85cb283a969fbb0a103dbd41dde9812d98827e
unknown <awalupshot@gmail.com>
2022-08-02 16:58:38 +0545

    asdasdasd

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # blogint
+
+to run the docker-compse.yml file folow this commands
+docker-compose run --rm app django-admin startproject core
+docker-compose up
+
+to build image on docker
+docker build --tag python-django .
+
+to publish it into a container
+docker run --publish 8000:8000 python-django
--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5z3&#op6-ilx60v8u3))l)236(70kfdm)gw2i*9@q)#vi63&-6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','poch-blog.herokuapp.com']
 
 
 # Application definition
